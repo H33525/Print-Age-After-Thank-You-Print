48fad5335a78e9f8042e7ee32f213879e0f1ead5
Hoshiyar Singh <hoshiyarhs9438625@gmail.com>
2020-09-03 22:09:45 +0530

    Add files via upload

---
+++ Print age after thank you print.py
@@ -0,0 +1,23 @@
+while True:
+
+try:
+ age= int(input('What is your age')
+
+10/age
+expect Value Error:
+print('please enter a number')
+continue
+
+expect ZeroDivisionError:
+  print('please enter age higher than 0')
+
+break
+
+else:
+  print('Thank You')
+
+finally:
+  print('OK, I am finally done')
+break
+
+print('Can you hear me? ')
